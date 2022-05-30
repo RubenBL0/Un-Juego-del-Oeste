30813679ab90d39922b3e1950f9f2dfd54d5a968
davidLM05 <davidlopezmontes05@gmail.com>
2022-05-30 21:18:07 +0200

    vaquilla script

---
+++ Assets/Scripts/MoverTaburete.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoverTaburete : MonoBehaviour
+{
+    public Transform salidaDisparo;
+    public Transform mira;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButton(1))
+        {
+            ReconocerObxecto();
+        }
+    }
+
+    public void ReconocerObxecto()
+    {
+        RaycastHit2D hit = Physics2D.Raycast(salidaDisparo.position, (mira.position - salidaDisparo.position).normalized, 100f);
+
+        if (hit.collider.gameObject.CompareTag("taburete"))
+        {
+            Vector3 mousePosition = Input.mousePosition;
+            mousePosition.z = 10;
+            hit.transform.position = Camera.main.ScreenToWorldPoint(mousePosition);
+        }
+
+    }
+}
